247b1a447c91e1b97113155a1960fc3ea5bc7e68
Freedom <ferudenis@gmail.com>
2026-04-07 09:34:56 +0100

    Shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,32 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test Square
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square Color: {square.GetColor()}, Area: {square.GetArea()}");
+
+        // Test Rectangle
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        // Test Circle
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"Circle Color: {circle.GetColor()}, Area: {circle.GetArea():F2}");
+
+        Console.WriteLine("\n--- Polymorphism in action with List<Shape> ---\n");
+
+        // Create a list of shapes (polymorphism)
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Yellow", 7));
+        shapes.Add(new Rectangle("Purple", 5, 8));
+        shapes.Add(new Circle("Orange", 4));
+
+        // Iterate through the list
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
